9f898a61032f6f583b0b3d25c270a520ade36cbe
nham <hamann.nick@gmail.com>
2015-03-01 02:38:50 -0600

    Implement fuzzy substring matching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::ops::{Index, IndexMut};
+use std::usize;
 
 fn main() {
     println!("{}", levenshtein("", ""));
@@ -9,6 +10,13 @@ fn main() {
     println!("{}", levenshtein("annually", "annealing"));
     println!("{}", levenshtein("0123456789 0123456789 0123456789 0123456789",
                                "0123456789 0a2345679  023456789 012233456789"));
+
+    println!("{}", fuzzy_contains("nana", "bananas", 0));
+    println!("{}", fuzzy_contains("I", "team", 0));
+    println!("{}", fuzzy_contains("bot", "zzzrocky bolboa", 1));
+    println!("{}", fuzzy_contains("zot", "zzzrocky bolboa", 1));
+    println!("{}", fuzzy_contains("annually", "simulated annealing", 2));
+    println!("{}", fuzzy_contains("annually", "simulated annealing", 3));
 }
 
 struct MemoMatrix<T> {
@@ -78,6 +86,62 @@ fn lev_rec<'a, 'b>(rect: &mut MemoMatrix<usize>, s: &'a [char], t: &'b [char]) -
             v.push(lev_rec(rect, &s[..a], &t[..b]));
             v.push(lev_rec(rect, &s[..a], t));
             v.push(lev_rec(rect, s, &t[..b]));
+            v.into_iter().min().unwrap() + 1
+        }
+    };
+
+    rect[(i, j)] = Some(dist);
+    dist
+}
+
+fn fuzzy_contains<'a, 'b>(p: &'a str, t: &'b str, k: usize) -> bool {
+    lev_substring(p, t) <= k
+}
+
+// returns the minimum edit distance between `p` and all substrings of `t`
+fn lev_substring<'a, 'b>(p: &'a str, t: &'b str) -> usize {
+    let p_chars: Vec<char> = p.chars().collect();
+    let t_chars: Vec<char> = t.chars().collect();
+
+    // M(i, j) is the minimal cost of an edit sequence that turns p[..i] into t[..j]
+    let n = t.chars().count();
+    let mut rect = MemoMatrix::new(p.chars().count() + 1, n + 1);
+
+    let mut min = usize::MAX;
+    for k in 0..(n+1) {
+        let dist = lev_sub_rec(&mut rect, &p_chars[], &t_chars[..k]);
+        if dist < min {
+            min = dist;
+        }
+    }
+
+    min
+}
+
+// the difference from lev_rec is M(0, j) = 0 for all j to ensure that there is no cost to
+// skipping ahead in the text string t
+fn lev_sub_rec<'a, 'b>(rect: &mut MemoMatrix<usize>, p: &'a [char], t: &'b [char]) -> usize {
+    // check if this has already been computed
+    let (i, j) = (p.len(), t.len());
+    match rect[(i, j)] {
+        Some(dist) => return dist,
+        None => {},
+    }
+
+    let dist = if i == 0 {
+        0
+    } else if j == 0 {
+        i
+    } else {
+        let (a, b) = (i-1, j-1);
+        if p[a] == t[b] {
+            // It can't be the case that M(i-1, j-1) > M(i-1, j) + 1 or M(i, j-1) + 1
+            lev_sub_rec(rect, &p[..a], &t[..b])
+        } else {
+            let mut v = vec![];
+            v.push(lev_sub_rec(rect, &p[..a], &t[..b]));
+            v.push(lev_sub_rec(rect, &p[..a], t));
+            v.push(lev_sub_rec(rect, p, &t[..b]));
             v.into_iter().min().unwrap() + 1
         }
     };
